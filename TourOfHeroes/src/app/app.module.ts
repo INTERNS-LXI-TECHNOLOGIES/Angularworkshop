a9677e462e18f9c683a5f7e54498397003984795
akhiljohnsonp <akhil.johnson.p@lxisoft.com>
2019-09-23 17:10:28 +0530

    IN-679

--- TourOfHeroes/src/app/app.module.ts
+++ TourOfHeroes/src/app/app.module.ts
@@ -1,6 +1,10 @@
+import { InMemoryDataService } from './in-memory-data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +12,9 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MesssagesComponent } from './messsages/messsages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClient } from 'selenium-webdriver/http';
+import { from } from 'rxjs';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +22,18 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MesssagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false}
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
