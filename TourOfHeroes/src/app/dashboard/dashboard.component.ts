22a190a8794f22949c7c8b9b3df420c6ad5dd47d
sanjanamuraliraj <sanjana.p@lxisoft.com>
2019-09-24 09:30:36 +0530

    IN-678

--- TourOfHeroes/src/app/dashboard/dashboard.component.ts
+++ TourOfHeroes/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero'
-import {HeroService} from '../hero.service'
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 
 @Component({
@@ -9,14 +9,14 @@ import {HeroService} from '../hero.service'
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  heroes:Hero[]=[];
+  heroes: Hero[] = [];
 
   constructor(private heroService:HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
-  
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5));
