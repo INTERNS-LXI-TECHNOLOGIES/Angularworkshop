c56b12a08f4a7341864f630639be0702c03b36f4
ayana98 <ayana.k.u@lxisoft.com>
2019-09-25 16:47:15 +0530

    IN-753

--- angular-quiz/src/app/app.component.ts
+++ angular-quiz/src/app/app.component.ts
@@ -11,8 +11,34 @@ export class AppComponent {
   languages: string[] = ['Java', 'Cprogram', 'HTML'];
   quizlist: Quizmodel[] = [
     {language: 'Java', question: 'Inventor of java', options: ['Nayan.c', 'Armesh', 'Denish Rechie', 'Kiran.Dy'], answer: 'Denish Rechie'},
+    {language: 'Java', question: 'Java is a collection of___', options: ['object', 'query', 'statement', 'data'], answer: 'object'},
+
     {language: 'Cprogram', question: 'Inventor of c program', options: ['Ken Thomson', 'Rob Pike', 'James', 'Richard'], answer: 'Rob Pike'},
     {language: 'HTML', question: 'Which tag is used to create table', options: ['div', 'th', 'html', 'table'], answer: 'table'}
   ];
   selectedvalue: string;
+  selectedlanques: any[];
+  question: string;
+  option: any[];
+  i: number;
+  quizlength: number;
+  gettinglanguage() {
+    this.selectedlanques =  this.quizlist.filter(d => (d.language === this.selectedvalue));
+    this.question = this.selectedlanques[0].question;
+    this.option = this.selectedlanques[0].options;
+    this.i = 0;
+    this.quizlength = this.selectedlanques.length;
+     }
+     next()
+     {
+       this.i++;
+       this.question = this.selectedlanques[this.i].question;
+       this.option = this.selectedlanques[this.i].options;
+     }
+     previous()
+     {
+       this.i--;
+       this.question = this.selectedlanques[this.i].question;
+       this.option = this.selectedlanques[this.i].options;
+     }
 }
