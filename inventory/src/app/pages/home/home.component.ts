f0ae6d1dcf8cdf2e1a438b29f32693919d1ddda6
greeshma01 <greeshma.c.g@lxisoft.com>
2019-10-03 17:00:51 +0530

    inventory

--- inventory/src/app/pages/home/home.component.ts
+++ inventory/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './../../service/product.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/Product';
+import { Router } from '@angular/router';
 
 @Component({
 selector: 'app-home',
@@ -9,21 +10,17 @@ styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 products: Product[] = [];
-constructor(private product: ProductService) { }
+constructor(private product: ProductService, private router: Router) { }
   ngOnInit() {
     this.product.getProducts().subscribe((products: Product[]) => this.products = products);
    }
    delete(id) {
    this.product.deleteProduct(id).subscribe();
-
    this.products = this.products.filter(data => data.id !== id );
-
    console.log('id', id);
   }
- update() {
-   this.product.updateProduct(this.product).subscribe();
-   console.log('FHHGHGGHGHGHGHH');
+
+ routeUpdate(product) {
+   this.router.navigate(['/create', product]);
  }
-
-
 }
