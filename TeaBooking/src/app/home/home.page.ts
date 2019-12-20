d8fe22c917ef839fcd3129c38df02fa65310b02b
sanjanamuraliraj <sanjana.p@lxisoft.com>
2019-12-20 16:42:22 +0530

    IN-3815

--- TeaBooking/src/app/home/home.page.ts
+++ TeaBooking/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,28 @@ import { Routes } from '@angular/router';
 
 
 export class HomePage {
+isImage = true;
+  constructor(public loadingController: LoadingController) {}
 
-  constructor() {}
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+    this.presentLoadingWithOptions();
+    this.changingValue();
+  }
 
+  changingValue() {
+    setTimeout(() => {
+      this.isImage = false;
+    }, 3000);
+  }
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      // spinner: 'lines-small',
+      duration: 2000,
+      // message: 'Please wait...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    return await loading.present();
+  }
 }
