e7227dd1d433893f5f082b4a616788b80127999d
greeshma01 <greeshma.c.g@lxisoft.com>
2019-12-28 16:08:58 +0530

    chaitime

--- chaitime/src/app/pages/booking/booking.page.ts
+++ chaitime/src/app/pages/booking/booking.page.ts
@@ -1,3 +1,4 @@
+import { Order } from './../../api/models/order';
 import { Beverage } from './../../api/models/beverage';
 import { OderingPipe } from 'src/app/pipes/odering.pipe';
 import { Component, OnInit } from '@angular/core';
@@ -11,13 +12,35 @@ import { BeverageResourceService } from 'src/app/api/services';
   styleUrls: ['./booking.page.scss'],
 })
 export class BookingPage implements OnInit {
-  count: number;
+  count = 0;
+  add: number[] = [];
+  order: Order;
   beverages: Beverage[] = [];
   constructor(public modalController: ModalController,
               private beverageResourceService: BeverageResourceService,
               private alert: AlertController
               ) { const oderingpipe = new OderingPipe(); }
   ngOnInit() {
-    this.beverageResourceService.getAllBeveragesUsingGET().subscribe(bev => this.beverages = bev);
+    this.beverageResourceService.getAllBeveragesUsingGET().subscribe(bev => {
+      this.beverages = bev;
+      bev.forEach(bev => {
+        this.add.push(0);
+      } );
+    });
+  }
+  createOder(beverage) {
+    const index = this.beverages.indexOf(beverage);
+    console.log(index);
+    this.add[index] = this.add[index] + 1;
+    this.count = this.count + 1;
+
+  }
+  subOder(beverage) {
+    const index = this.beverages.indexOf(beverage);
+    this.add[index] = this.add[index] - 1;
+  }
+
+  buy() {
+
   }
 }
