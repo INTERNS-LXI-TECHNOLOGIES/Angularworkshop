7033f70cdd50c732b093157c4a24d958824f1855
pushkala-m <pushkala.m@lxisoft.com>
2020-05-07 11:58:22 +0530

    list of heroes and list

---
+++ TourOfHeroes/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+export const HEROES:Hero[]=[
+    {id:11,name:'Jerry'},
+    {id:12,name:'Bubbly'},
+    {id:13,name:'Monu'},
+    {id:14,name:'Pushku'},
+    {id:15,name:'Aji'},
+    {id:16,name:'Ashiq'},
+    {id:17,name:'Mehru'},
+    {id:18,name:'Abhi'},
+    {id:19,name:'Jose'},
+    {id:20,name:'suji'}
+];
+    
