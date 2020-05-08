57baf9cc90bc4734f8d18e15f998174c4d14bc93
abhijith <abhijith.v.m@lxisoft.com>
2020-05-08 20:16:55 +0530

    Display by List

--- TourOfHeroes/sample-project/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/sample-project/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,9 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
-
+  heroes = HEROES;
   ngOnInit() {
   }
-
 }
