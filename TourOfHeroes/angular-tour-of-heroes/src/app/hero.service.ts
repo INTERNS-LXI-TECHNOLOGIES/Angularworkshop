189ec4ad20a72ebf7e666bfc108163fbf7223356
muhammedashiq <muhammed.ashiq.k.p@lxisoft.com>
2020-05-08 19:16:56 +0530

    service

---
+++ TourOfHeroes/angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+ getHeroes(): Observable<Hero[]> {
+  return of(HEROES);
+}
+  constructor() { }
+}
