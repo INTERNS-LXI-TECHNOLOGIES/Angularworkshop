d99feee278239f9b1d89d2f5d2ed08bf6dff0b21
muhammedashiq <muhammed.ashiq.k.p@lxisoft.com>
2020-05-09 16:52:58 +0530

    messageComponent

--- TourOfHeroes/angular-tour-of-heroes/src/app/hero.service.ts
+++ TourOfHeroes/angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
 import { HEROES } from './mock-heroes';
-
+import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
 
  getHeroes(): Observable<Hero[]> {
   return of(HEROES);
 }
-  constructor() { }
+
+constructor(private messageService: MessageService) { }
+  
 }
